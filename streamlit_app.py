2f780182d11a4006f1a789509a8a5b2a868b547f
PeterYu789 <160038108+PeterYu789@users.noreply.github.com>
2024-06-19 09:29:46 +0000

    update the interface

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,18 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import zipfile, pymupdf, re, os
+import zipfile, pymupdf, re, os, tempfile
 from docx import Document
 
-tree_data = []
+df_data = {
+    "Standard": [],
+    "Matching Number": [],
+    "Filename": [],
+}
 
 def get_standard_names():
     standard_names = os.listdir('standards/')
+
     return standard_names
 
 def zip_outputs():
@@ -20,6 +25,19 @@ def clear_folder():
     files = os.listdir('outputs/')
     for file in files:
         os.remove(os.path.join('outputs/', file))
+
+def get_match_data():
+    df_data['Standard'] = []
+    df_data['Matching Number'] = []
+    df_data['Filename'] = []
+    files = os.listdir('outputs/')
+    for file in files:
+        list_name, matching_number, filename = file.split('*')
+        if matching_number != '0':
+            df_data['Standard'].append(list_name)
+            df_data['Matching Number'].append(matching_number)
+            df_data['Filename'].append(filename)
+    return pd.DataFrame(df_data)
 
 class CASMatcher:
     def __init__(self):
@@ -61,7 +79,10 @@ class CASMatcher:
         return df
 
     def get_MDSReport_pdf(self, report):
-        doc = pymupdf.Document(report)
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+        temp_file.write(report.read())
+        temp_file.close()
+        doc = pymupdf.Document(temp_file.name)
         dfs = []
         for page in doc:
             tabs = page.find_tables()
@@ -108,18 +129,18 @@ class CASMatcher:
         result = result[result.iloc[:, 2].apply(self.find_numeric_hyphen_strings)]
         result_Found = pd.DataFrame(yielded_rows[::-1])
         filename = report.name.split('/')[-1].split('.')[0] + '.xlsx'
-        filepath = 'outputs/' + standard_name.split('_')[-1].split('.')[0] + '_' + str(count) + "_" + filename
+        filepath = 'outputs/' + standard_name.split('_')[-1].split('.')[0] + '*' + str(count) + "*" + filename
         with pd.ExcelWriter(filepath, engine='openpyxl') as writer:
             result.to_excel(writer, sheet_name="Total List", index=False)
             if count:
                 result_Found.to_excel(writer, sheet_name="Summary", index=False)
-                tree_data.append((standard_name.split('_')[-1].split('.')[0], str(count), filename))
             self.fit_excel(writer)
 
 def main():
     matcher = CASMatcher()
+    st.set_page_config(page_title='CASMatcher', page_icon='https://www.johnsonelectric.com/pub/media/favicon/stores/1/johnson.jpg')
     st.title('CASMatcher Application')
-    st.header('1. Upload the standard lists for comparison:')
+    st.header('1. Upload the standard lists for comparison:', divider='rainbow')
     standard_lists = st.file_uploader(
         label='Upload Standard Lists',
         type=["xlsx"],
@@ -128,40 +149,68 @@ def main():
     if standard_lists is not None:
         for standard_list in standard_lists:
             matcher.preprocess_compareList(standard_list)
-    st.header('2. Choose the list for matching:')
+        standard_lists = None
+    st.header('2. Choose the list for matching:', divider='rainbow')
     standard_names = get_standard_names()
     standard_name = st.selectbox(
         label='Select Standard List',
         options=standard_names,
         )
-    st.header('3. Upload the MAS reports:')
     if standard_name is not None:
+        st.header('3. Upload the MAS reports:', divider='rainbow')
         MDSreports = st.file_uploader(
             label='Upload MAS Reports',
             type=["docx", "pdf"],
             accept_multiple_files=True,
             )
-        if MDSreports is not None:
-            for report in MDSreports:
-                matcher.get_result(report, standard_name)
-        zip_outputs()
+        _, _, body_col3, _, _ = st.columns(5)
+        with body_col3:
+            process = st.button("Process")
+        progress_bar = st.progress(0, text="")
+        if process and MDSreports is not None:
+            if len(MDSreports) == 0:
+                st.text("No reports are uploaded.")
+            else:
+                num = len(MDSreports)
+                for i, report in enumerate(MDSreports):
+                    matcher.get_result(report, standard_name)
+                    progress_bar.progress(1*(i+1)/num, text="Operation in progress. Please wait.")
+                progress_bar.progress(1., text="Operation Finished.")
+            MDSreports = None
+        st.divider()
+        end_col1, end_col2 = st.columns(2)
+        with end_col1:
+            zip_outputs()
+            st.subheader("Download the output files:")
+            with open('outputs.zip', 'rb') as datazip:
+                st.download_button(
+                    label='Download ZIP',
+                    data=datazip,
+                    file_name="outputs.zip",
+                    mime="application/octet-stream"
+                    )
+            st.caption('Successfully Downloaded.')
+        with end_col2:
+            st.subheader("Clear the output files:")
+            st.button(label='Clear Outputs', on_click=clear_folder)
+            st.caption('Successfully Cleared.')
+        st.subheader("List of files with matching CAS number:")
+        st.dataframe(
+            get_match_data(),
+            hide_index=True,
+        )
     else:
         st.caption('Please upload and select standard list for further actions.')
-    col1, col2 = st.columns(2)
-    with col1:
-        st.subheader("Download the output files:")
-        with open('outputs.zip', 'rb') as datazip:
-            st.download_button(
-                label='Download ZIP',
-                data=datazip,
-                file_name="outputs.zip",
-                mime="application/octet-stream"
-                )
-    with col2:
-        st.subheader("Clear the output files:")
-        if st.button(label='Clear Outputs'):
-            clear_folder()
-
+    
+    with st.sidebar:
+        st.header('Guideline')
+        st.markdown('**1. Upload standard lists that you want to use for comparison.**')
+        st.markdown('**2. Select the standard list for comparison.**')
+        st.markdown('**3. Upload the MAS Reports before process.**')
+        st.caption('*Please wait until all reports are uploaded for next step.')
+        st.markdown('**4. Press the "Process" button.**')
+        st.caption('*Wait until operation success.')
+        st.markdown('**5. Download the output files via "Download ZIP" button.**')
 
 if __name__ == '__main__':
     main()
